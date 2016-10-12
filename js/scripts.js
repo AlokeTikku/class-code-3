4211a816b1d584e8a98616b1e802b9931a7b278c
Aloke Tikku (HT Editorial <M72903@htmedia.in>
2016-10-12 17:55:45 +0530

    created variables with diff data types

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
 //This is to add comment. This is my javascript. I want to see if it is working
-
-console.log("My javascript is working.");
+//console.log("My javascript is working.");
+//In Javascript, there are five basic data types
+// A string which is a sequence of characters
+var myString = "Hello world.";
+// console.log(myString);
+// 2) A number
+var myNumber = 24;
+// console.log(myNumber);
+// console.log(myNumber+70);
+// console.log(myString+70);
+// console.log(myNumber.toString() +70)
+// 3) An array, which is a fancy word for a List array are comma separated
+var myArray = ["Prawesh", "Aloke", "Anand", "Sansey", "Garima"];
+// console.log(myArray);
+// console.log(myArray[3]);
+// console.log(myArray.length);
+myArray.push("Harry");
+// console.log(myArray);
+myArray.push(myNumber);
+// console.log(myArray);
+// 4) An object, which is a set of key-value pairs or Key property pairs
+//To comment on any line, ctrl slash.. it wont show
+var myObject = {
+  name: "Harry",
+age: 29,
+classmates: myArray
+}
+myObject.classmates [2]
+// 5) Boolean, which is true or false
+var myBool = true;
